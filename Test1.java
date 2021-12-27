12d98a023645b432296ade9bad74fde5dbf75f2e
sharabanys <82096325+sharabanys@users.noreply.github.com>
2021-12-27 11:10:50 +0200

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -9,4 +9,6 @@ public class CxIntegrationTest
 		Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 	     
 		ResultSet results = statement.executeQuery(query);
+	     	ResultSet results1 = statement.executeQuery(query);
+
      }
